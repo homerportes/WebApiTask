1f4b74487cd36b37d7b4fa666ed30d19f47c598f
Homer <homerportes@gmail.cpm>
2025-05-07 17:38:53 -0400

    Base del crud funcionando bien

--- WebApiTask/Program.cs
+++ WebApiTask/Program.cs
@@ -1,17 +1,40 @@
+using ApplicationLayer.Services.TaskServices;
+using DomainLayer.Models;
+using InfrastuctureLayer;
+using InfrastuctureLayer.Repositorio.Commons;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<WebApiTaskContext>(options =>
+{
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TaskManagerDB"));
+
+});
+
+
+builder.Services.AddScoped<ICommonsProcess<Tareas>, TaskRepository>();
+builder.Services.AddScoped<TaskServices>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<WebApiTaskContext>();
+    context.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); 
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
