ade087a58c6651d1bf8cbf8b99d17a7e60fc1968
Homer <homerportes@gmail.cpm>
2025-05-07 23:15:42 -0400

    Reapply "Base del crud funcionando bien"
    This reverts commit 80b60d5ad13ca9397fd2650023a8fb670d0a0950.

---
+++ WebApiTask/Controllers/TareasController.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationLayer.Services.TaskServices;
+using DomainLayer.DTO;
+using DomainLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly TaskServices _service;
+
+        public TareasController(TaskServices service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Response<Tareas>>> GetTaskAllAsync()
+            => await _service.GetTaskAllAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Response<Tareas>>> GetTaskByIdAllAsync(int id)
+            => await _service.GetTaskByIdAllAsync(id);
+
+        [HttpPost]
+        public async Task<ActionResult<Response<string>>> AddTaskAllAsync(Tareas tarea)
+            => await _service.AddTaskAllAsync(tarea);
+
+        [HttpPut]
+        public async Task<ActionResult<Response<string>>> UpdateTaskAllAsync(Tareas tarea)
+        => await _service.UpdateTaskAllAsync(tarea);
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Response<string>>> DeleteTaskAllAsync(int id)
+         => await _service.DeleteTaskAllAsync(id);
+
+    }
+}
