8c29d3370eb332e1d9e3069812d1aa5f9544432c
Homer <homerportes@gmail.cpm>
2025-05-15 18:11:11 -0400

    Revert "antes de añadir el factory"
    This reverts commit 87da7688eabf0b012915b4bbd738a525d671e6d7.

--- WebApiTask/Controllers/TareasController.cs
+++ WebApiTask/Controllers/TareasController.cs
@@ -59,7 +59,7 @@ namespace WebApiTask.Controllers
         }
 
         [HttpGet("{id}")]
-        [ActionName("GetTaskById")]z
+        [ActionName("GetTaskById")]
         public async Task<ActionResult<Response<Tareas>>> GetTaskByIdAllAsync(int id)
         {
             if (id <= 0)
